e41b4df0fb42cd16f1e3c7b65deb39ffc205a5e3
mhusnainraza512 <mhusnainraza512>
2021-07-10 14:05:58 +0500

    first commit

--- resources/views/customer/layouts/header.blade.php
+++ resources/views/customer/layouts/header.blade.php
@@ -27,13 +27,14 @@
                 <span class="avatar avatar-online">
                   <img src="{{ asset(Auth::user()->image) ?? '' }}" alt="avatar"><i></i></span>
               </a>
-              <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i class="ft-user"></i> Edit Profile</a>
-                <a class="dropdown-item" href="#"><i class="ft-mail"></i> My Inbox</a>
+              <div class="dropdown-menu dropdown-menu-right">
+              {{-- <a class="dropdown-item" href="#"><i class="ft-user"></i> Edit Profile</a> --}}
+                {{-- <a class="dropdown-item" href="#"><i class="ft-mail"></i> My Inbox</a>
                 <a class="dropdown-item" href="#"><i class="ft-check-square"></i> Task</a>
-                <a class="dropdown-item" href="#"><i class="ft-message-square"></i> Chats</a>
-                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#"><i class="ft-message-square"></i> Chats</a> --}}
+                {{-- <div class="dropdown-divider"></div> --}}
                 <form action="{{ route('logout') }}" method="POST">
-                  <button type="submit" class="dropdown-item"><i class="ft-power"></i>Logout</button>
+                  <button style="cursor: pointer;" type="submit" class="dropdown-item"><i class="ft-power"></i>Logout</button>
                 </form>
               </div>
             </li>
